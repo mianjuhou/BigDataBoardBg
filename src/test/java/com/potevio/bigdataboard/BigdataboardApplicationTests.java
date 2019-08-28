ad53677e4557823b667e8fd0443603c6d88076a9
fda <fangdean@yeah.net>
2019-08-28 13:50:08 +0800

    添加上前端页面

--- src/test/java/com/potevio/bigdataboard/BigdataboardApplicationTests.java
+++ src/test/java/com/potevio/bigdataboard/BigdataboardApplicationTests.java
@@ -1,25 +1,17 @@
 package com.potevio.bigdataboard;
 
-import com.potevio.bigdataboard.service.MainDataService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.lang.reflect.Array;
-import java.util.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BigdataboardApplicationTests {
 
-    @Autowired
-    private MainDataService service;
-
     @Test
     public void contextLoads() {
-//        service.getTeacherTitle();
+
     }
 
 
